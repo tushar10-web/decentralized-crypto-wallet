5265590b7a8dd6760c8982938f0bfd219727690b
Tushar Yadav <tusharr.8487@gmail.com>
2026-05-07 11:20:36 +0530

    Connected to Online DB
    # Conflicts:
    #	src/main/resources/application.properties

--- src/main/java/com/wallet/cyptowallet/CyptowalletBackendApplication.java
+++ src/main/java/com/wallet/cyptowallet/CyptowalletBackendApplication.java
@@ -7,7 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CyptowalletBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CyptowalletBackendApplication.class, args);
-    }
 
+        System.setProperty(
+                "spring.data.mongodb.uri",
+                "mongodb+srv://devyadav8487_db_user:Tushar123@cluster0.sskkgq9.mongodb.net/cryptowallet?retryWrites=true&w=majority&appName=Cluster0"
+        );
+
+        SpringApplication.run(
+                CyptowalletBackendApplication.class,
+                args
+        );
+    }
 }
